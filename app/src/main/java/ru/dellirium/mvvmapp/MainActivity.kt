2491cf886f517e9ec8c6fa9d6fdc34991036df6d
Dellirium <dellirium@dellirium.ru>
2020-06-02 18:21:11 +0300

    add observers via data binding

--- app/src/main/java/ru/dellirium/mvvmapp/MainActivity.kt
+++ app/src/main/java/ru/dellirium/mvvmapp/MainActivity.kt
@@ -1,11 +1,21 @@
 package ru.dellirium.mvvmapp
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import ru.dellirium.mvvmapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        //val viewModel: MainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        val viewModel: MainViewModel by viewModels()
+        val binding: ActivityMainBinding = DataBindingUtil
+                .setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
     }
 }
